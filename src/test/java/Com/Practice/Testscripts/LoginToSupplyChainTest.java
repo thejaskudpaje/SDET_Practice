b0bce4e2061f6c0b2680234194aac0bb80e2186d
thejaskudpaje1996@gmail.com <thejaskudpaje1996@gmail.com>
2023-07-12 17:27:20 +0530

    04

--- src/test/java/Com/Practice/Testscripts/LoginToSupplyChainTest.java
+++ src/test/java/Com/Practice/Testscripts/LoginToSupplyChainTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-public class LoginToSupplyChain {
+public class LoginToSupplyChainTest {
 //@Parameters("BROWSER")
 @Test
 public void loginAsAdminTest() {
